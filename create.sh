4004c974810328040bc62db09aa3d85fc005392a
Matthew Gilpin <matty0005@gmail.com>
2021-01-29 12:08:40 +1000

    Added support for linux/macos

--- create.sh
+++ create.sh
@@ -19,7 +19,14 @@ curl -sO https://raw.githubusercontent.com/matty0005/pi-pico-templates/main/proj
 mv project.c $NAME.c
 
 curl -sO https://raw.githubusercontent.com/matty0005/pi-pico-templates/main/CMakeLists.txt
-sed -i '' "s/project_name/$NAME/g" CMakeLists.txt
+
+# Set the CMakeLists to use the project name
+os=`uname`
+if [[ "$os" == 'Darwin' ]]; then
+   sed -i '' "s/project_name/$NAME/g" CMakeLists.txt
+else 
+   sed -i "s/project_name/$NAME/g" CMakeLists.txt
+fi
 
 echo "~ Done"
 echo "~ run './build.sh' to build"
